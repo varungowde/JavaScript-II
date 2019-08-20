945321c4b22ef6744133c4171488105f430696a1
vm1821 <vm1821@jagmail.southalabama.edu>
2019-08-20 06:38:19 -0500

    array methods assignments done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -58,28 +58,36 @@ const runners = [
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs. Combine both the first and last names and populate a new array called `fullNames`. This array will contain just strings.
 let fullNames = [];
+runners.forEach(r => fullNames.push(r.first_name + " " + r.last_name))
 console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runners' first names in uppercase because the director BECAME DRUNK WITH POWER. Populate an array called `firstNamesAllCaps`. This array will contain just strings.
-let firstNamesAllCaps = [];
+let firstNamesAllCaps = runners.map(r => r.first_name.toUpperCase())
 console.log(firstNamesAllCaps);
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue. We need a filtered version of the runners array, containing only those runners with large sized shirts so they can choose a different size. This will be an array of objects.
-let runnersLargeSizeShirt = [];
+let runnersLargeSizeShirt = runners.filter(r => r.shirt_size === "L");
 console.log(runnersLargeSizeShirt);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations and save the total into a ticketPriceTotal variable.
-let ticketPriceTotal = 0;
+let ticketPriceTotal = runners.reduce((t, item) => t + parseFloat(item.donation), 0);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
 // Problem 1
-
+// All small sized shirts donation 
+var smallShirtsDonation = runners.filter(r => r.shirt_size == "S").reduce((t, i) => t + parseFloat(i.donation), 0)
+console.log(smallShirtsDonation)
 // Problem 2
-
+// All the emails that have donation greater than 200
+var mailsDonation = runners.filter(r => parseFloat(r.donation) > 200).map(r => r.email);
+console.log(mailsDonation)
 // Problem 3
+// find avg donation for each person with size as M
+var avg_donation = runners.filter(r => r.shirt_size == "M").reduce((t, i) => t + parseFloat(i.donation), 0) / runners.filter(r => r.shirt_size == "M").length
+console.log(avg_donation);
